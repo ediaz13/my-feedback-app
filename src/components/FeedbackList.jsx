c11a12f742c70d7e645bcd41b1482bd27854a596
ediaz13 <diazemanuel27@gmail.com>
2023-02-27 09:35:14 -0300

    16. Card Component & Conditional Styles

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import PropTypes from 'prop-types'
 import FeedbackItem from "./FeedbackItem";
 
 function FeedbackList({feedback}) {
@@ -11,6 +11,16 @@ function FeedbackList({feedback}) {
         <FeedbackItem key={item.id} item={item}/>
         ))}
     </div>
+  )
+}
+
+FeedbackList.propTypes = {
+  feedback: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+      rating: PropTypes.number.isRequired,
+    })
   )
 }
 
