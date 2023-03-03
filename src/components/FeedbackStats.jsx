f94875151e9d9f4d0208890ece786282d2fe6c2e
ediaz13 <diazemanuel27@gmail.com>
2023-03-02 23:11:07 -0300

    18. FeedbackStats Component & Reactivity

--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import PropTypes  from 'prop-types';
 
-export default function FeedbackStats() {
-  return (
-    <div>Stats</div>
+function FeedbackStats({feedback}) {
+  
+    let average = feedback.reduce((accumulator, current) => {
+        return accumulator + current.rating;
+    }, 0) / feedback.length;
+
+    average = average.toFixed(1).replace(/[.,]0$/, '')
+
+    return (
+    <div className='feedback-stats'>
+        <h4>{feedback.length} Reviews</h4>
+        <h4>Average Rating: {isNaN(average) ? 0 : average}</h4>
+    </div>
   )
 }
+
+FeedbackStats.protoTypes = {
+    feedback: PropTypes.array.isRequired
+}
+
+ export default FeedbackStats;
