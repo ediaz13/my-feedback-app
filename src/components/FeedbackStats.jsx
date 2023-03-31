7b4f66c469b6da665cc0c50944ee1ba107d36151
ediaz13 <diazemanuel27@gmail.com>
2023-03-31 20:12:52 -0300

    31. Get Global State With The useContext Hook

--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import PropTypes  from 'prop-types';
+import { useContext } from 'react'
+import FeedbackContext from '../context/FeedbackContext'
 
-function FeedbackStats({feedback}) {
-  
+function FeedbackStats() {
+
+    const {feedback} = useContext(FeedbackContext)
+
     let average = feedback.reduce((accumulator, current) => {
         return accumulator + current.rating;
     }, 0) / feedback.length;
