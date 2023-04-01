f95430df55d05525c754bb4656d8d5e959534529
ediaz13 <diazemanuel27@gmail.com>
2023-04-01 19:28:12 -0300

    33. Edit Feedback Event

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -21,6 +21,11 @@ export const FeedbackProvider = ({children}) => {
             rating: 10
         }
     ])
+
+    const [FeedbackEdit, setFeedbackEdit] = useState({
+        item: {},
+        edit: false
+    })
     
     const addFeedback = (newFeedback) => {
       newFeedback.id = uuidv4();
@@ -34,10 +39,20 @@ export const FeedbackProvider = ({children}) => {
       }
     };
 
+
+    //Set item to updated
+    const editFeedback = (item) => {
+        setFeedback({
+            item,
+            edit:true 
+        })
+      };
+
     return <FeedbackContext.Provider value={{
         feedback,
         deleteFeedback,
         addFeedback,
+        editFeedback,
     }}>
         {children}
     </FeedbackContext.Provider>
