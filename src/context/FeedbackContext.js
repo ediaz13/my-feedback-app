4d7e4d35b750086434ff765b96d298cc13ba7ca5
ediaz13 <diazemanuel27@gmail.com>
2023-04-05 19:45:37 -0300

    40. Fetch Data From JSON-Server Backend

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,31 +1,19 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 import { v4 as uuidv4 } from 'uuid'
 
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({children}) => {
-    const [feedback, setFeedback] = useState ([
-        {
-            id: 1,
-            text: 'This item is from context 1',
-            rating: 10
-        },
-        {
-            id: 2,
-            text: 'This item is from context 2',
-            rating: 10
-        },
-        {
-            id: 3,
-            text: 'This item is from context 3',
-            rating: 10
-        }
-    ])
+    const [feedback, setFeedback] = useState ([])
 
     const [feedbackEdit, setFeedbackEdit] = useState({
         item: {},
         edit: false
     })
+
+    useEffect(() => {
+        console.log(123)
+    }, [])
     
     const addFeedback = (newFeedback) => {
       newFeedback.id = uuidv4();
