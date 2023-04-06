118b9e0d9adb92f9139d4c5bd0875618c4b5f658
ediaz13 <diazemanuel27@gmail.com>
2023-04-06 15:43:32 -0300

    40. Fetch Data From JSON-Server Backend

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -4,16 +4,27 @@ import { v4 as uuidv4 } from 'uuid'
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({children}) => {
+    const [isLoading, setIsLoading] = useState (true)
     const [feedback, setFeedback] = useState ([])
-
     const [feedbackEdit, setFeedbackEdit] = useState({
         item: {},
         edit: false
     })
 
     useEffect(() => {
-        console.log(123)
+        fetchFeedback()
     }, [])
+
+    //Fetch Feedback
+    const fetchFeedback = async () => {
+        const response = await fetch(`http://localhost:5000/feedback?_sort=id&_order=desc`)
+
+        const data = await response.json()
+
+        console.log(data)
+
+        setIsLoading(faile);
+    }
     
     const addFeedback = (newFeedback) => {
       newFeedback.id = uuidv4();
@@ -42,6 +53,7 @@ export const FeedbackProvider = ({children}) => {
     return <FeedbackContext.Provider value={{
         feedback,
         feedbackEdit,
+        isLoading,
         deleteFeedback,
         addFeedback,
         editFeedback,
